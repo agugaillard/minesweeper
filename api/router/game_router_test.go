ac837a7c0c26acbf281fd5fbb142b41b1a052330
agu <agudevelop@gmail.com>
2020-10-26 22:22:09 -0300

    Add more tests

--- api/router/game_router_test.go
+++ api/router/game_router_test.go
@@ -47,7 +47,6 @@ func TestExploreMine(t *testing.T) {
 	r := gin.Default()
 	gameRouter := GameRouter{GameService: GameServiceTest}
 	gameRouter.Routes(r)
-
 	game, _ := gameRouter.GameService.New(3, 3, 2, "user")
 	w := httptest.NewRecorder()
 	body, _ := json.Marshal(dto.ExploreCellRequestDto{
@@ -68,13 +67,15 @@ func TestExploreMine(t *testing.T) {
 	if !response.Finished {
 		t.Errorf("game should have finished, mine was explored")
 	}
+	if response.Board.Solved {
+		t.Errorf("game shouldn't have been solved, mine was explored")
+	}
 }
 
 func TestExploreWinGame(t *testing.T) {
 	r := gin.Default()
 	gameRouter := GameRouter{GameService: GameServiceTest}
 	gameRouter.Routes(r)
-
 	game, _ := gameRouter.GameService.New(5, 5, 1, "user")
 	w := httptest.NewRecorder()
 	body, _ := json.Marshal(dto.ExploreCellRequestDto{
@@ -95,13 +96,15 @@ func TestExploreWinGame(t *testing.T) {
 	if !response.Finished {
 		t.Errorf("game should have finished, all free cell were explored")
 	}
+	if !response.Board.Solved {
+		t.Errorf("game should have been solved, all free cell were explored")
+	}
 }
 
 func TestFlagCell(t *testing.T) {
 	r := gin.Default()
 	gameRouter := GameRouter{GameService: GameServiceTest}
 	gameRouter.Routes(r)
-
 	game, _ := gameRouter.GameService.New(5, 5, 1, "user")
 	w := httptest.NewRecorder()
 	body, _ := json.Marshal(dto.FlagCellRequestDto{
@@ -118,6 +121,126 @@ func TestFlagCell(t *testing.T) {
 
 	if game.Board.Cells[4][4].Flag != model.RedFlag {
 		t.Errorf("redflag was expected")
+	}
+}
+
+func TestExploreFlagged(t *testing.T) {
+	r := gin.Default()
+	gameRouter := GameRouter{GameService: GameServiceTest}
+	gameRouter.Routes(r)
+	game, _ := gameRouter.GameService.New(5, 5, 1, "user")
+	w := httptest.NewRecorder()
+	body, _ := json.Marshal(dto.FlagCellRequestDto{
+		Position: model.Position{Col: 0, Row: 0},
+		Username: "user",
+	})
+	req, _ := http.NewRequest("PUT", "/game/"+game.Id+"/flag", bytes.NewBuffer(body))
+	r.ServeHTTP(w, req)
+	if w.Code != 200 {
+		t.Errorf("unexpected status code, found " + strconv.Itoa(w.Code) + ", 200 was expected")
+	}
+
+	w = httptest.NewRecorder()
+	body, _ = json.Marshal(dto.ExploreCellRequestDto{
+		Position: model.Position{Col: 0, Row: 0},
+		Username: "user",
+	})
+	req, _ = http.NewRequest("POST", "/game/"+game.Id+"/explore", bytes.NewBuffer(body))
+	r.ServeHTTP(w, req)
+	if w.Code != 400 {
+		t.Errorf("unexpected status code, found " + strconv.Itoa(w.Code) + ", 400 was expected")
+	}
+
+	var response dto.GameResponseDto
+	err := json.Unmarshal(w.Body.Bytes(), &response)
+	if err != nil {
+		t.Errorf("error deserializing response")
+	}
+	if response.Finished {
+		t.Errorf("game should have continue, can't explore flagged cell")
+	}
+}
+
+func TestResume(t *testing.T) {
+	r := gin.Default()
+	gameRouter := GameRouter{GameService: GameServiceTest}
+	gameRouter.Routes(r)
+	game, _ := gameRouter.GameService.New(5, 5, 1, "user")
+	w := httptest.NewRecorder()
+	body, _ := json.Marshal(dto.FlagCellRequestDto{
+		Position: model.Position{Col: 4, Row: 4},
+		Flag:     model.RedFlag,
+		Username: "user",
+	})
+	req, _ := http.NewRequest("PUT", "/game/"+game.Id+"/flag", bytes.NewBuffer(body))
+	r.ServeHTTP(w, req)
+	w = httptest.NewRecorder()
+	body, _ = json.Marshal(dto.ResumeGameRequestDto{
+		Username: "user",
+	})
+	req, _ = http.NewRequest("POST", "/game/"+game.Id+"/resume", bytes.NewBuffer(body))
+	r.ServeHTTP(w, req)
+	if w.Code != 200 {
+		t.Errorf("unexpected status code, found " + strconv.Itoa(w.Code) + ", 200 was expected")
+	}
+	var response dto.GameResponseDto
+	err := json.Unmarshal(w.Body.Bytes(), &response)
+	if err != nil {
+		t.Errorf("error deserializing response")
+	}
+	if response.Board.Cells[24].Flag != model.RedFlag {
+		t.Errorf("this cell should be flagged with red_flag")
+	}
+}
+
+func TestExploreAuth(t *testing.T) {
+	r := gin.Default()
+	gameRouter := GameRouter{GameService: GameServiceTest}
+	gameRouter.Routes(r)
+	game, _ := gameRouter.GameService.New(5, 5, 1, "user")
+	w := httptest.NewRecorder()
+	body, _ := json.Marshal(dto.ExploreCellRequestDto{
+		Position: model.Position{Col: 4, Row: 4},
+		Username: "not_user",
+	})
+	req, _ := http.NewRequest("POST", "/game/"+game.Id+"/explore", bytes.NewBuffer(body))
+	r.ServeHTTP(w, req)
+	if w.Code != 403 {
+		t.Errorf("user shouldn't be able to explore in this game")
+	}
+}
+
+func TestFlagAuth(t *testing.T) {
+	r := gin.Default()
+	gameRouter := GameRouter{GameService: GameServiceTest}
+	gameRouter.Routes(r)
+	game, _ := gameRouter.GameService.New(5, 5, 1, "user")
+	w := httptest.NewRecorder()
+	body, _ := json.Marshal(dto.FlagCellRequestDto{
+		Position: model.Position{Col: 4, Row: 4},
+		Flag:     model.RedFlag,
+		Username: "not_user",
+	})
+	req, _ := http.NewRequest("PUT", "/game/"+game.Id+"/flag", bytes.NewBuffer(body))
+	r.ServeHTTP(w, req)
+	if w.Code != 403 {
+		t.Errorf("user shouldn't be able to flag in this game")
+	}
+}
+
+func TestResumeAuth(t *testing.T) {
+	r := gin.Default()
+	gameRouter := GameRouter{GameService: GameServiceTest}
+	gameRouter.Routes(r)
+	game, _ := gameRouter.GameService.New(5, 5, 1, "user")
+	w := httptest.NewRecorder()
+	body, _ := json.Marshal(dto.ResumeGameRequestDto{
+		Username: "not_user",
+	})
+	req, _ := http.NewRequest("POST", "/game/"+game.Id+"/resume", bytes.NewBuffer(body))
+	r.ServeHTTP(w, req)
+	if w.Code != 403 {
+		t.Errorf("user shouldn't be able to resume this game")
 	}
 }
 
