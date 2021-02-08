2835b3ffbf85f54da95a0d9402c21014c4b2138f
agu <agaillard@fi.uba.ar>
2021-02-08 02:05:58 -0300

    Improve curl

--- curl_test.sh
+++ curl_test.sh
@@ -1,13 +1,18 @@
-# New game
-curl -X POST "http://18.228.204.155/game" -H "accept: application/json" -H "Content-Type: application/json" -d "{\"cols\": 5, \"rows\": 5, \"mines\": 8, \"username\": \"myuser\"}" -v
+host="http://52.67.218.214"
 
-# 112783cd-66bd-4a85-9445-1ca293c41c25 is a valid id, and is used for the rest of the tests
+# Health check
+curl "$host/health-check" -v
+
+# New game
+curl -X POST "$host/game" -H "accept: application/json" -H "Content-Type: application/json" -d "{\"cols\": 5, \"rows\": 5, \"mines\": 8, \"username\": \"myuser\"}" -v
+
+gameid="58e9745e-4874-4449-a39b-a49d2e61bc84"
 
 # Flag
-curl -X PUT "http://18.228.204.155/game/112783cd-66bd-4a85-9445-1ca293c41c25/flag" -H "accept: application/json" -H "Content-Type: application/json" -d "{\"col\": 0, \"row\": 0, \"flag\": \"red_flag\", \"username\": \"myuser\"}" -v
+curl -X PUT "$host/game/$gameid/flag" -H "accept: application/json" -H "Content-Type: application/json" -d "{\"col\": 0, \"row\": 0, \"flag\": \"red_flag\", \"username\": \"myuser\"}" -v
 
 # Explore
-curl -X POST "http://18.228.204.155/game/112783cd-66bd-4a85-9445-1ca293c41c25/explore" -H "accept: application/json" -H "Content-Type: application/json" -d "{\"col\": 0, \"row\": 0, \"username\": \"myuser\"}" -v
+curl -X POST "$host/game/$gameid/explore" -H "accept: application/json" -H "Content-Type: application/json" -d "{\"col\": 0, \"row\": 0, \"username\": \"myuser\"}" -v
 
 # Resume game
-curl -X POST "http://18.228.204.155/game/112783cd-66bd-4a85-9445-1ca293c41c25/resume" -H "accept: application/json" -H "Content-Type: application/json" -d "{\"username\": \"myuser\"}" -v
+curl -X POST "$host/game/$gameid/resume" -H "accept: application/json" -H "Content-Type: application/json" -d "{\"username\": \"myuser\"}" -v
