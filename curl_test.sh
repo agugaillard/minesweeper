37f8d226d0cd661497b317fcbcc6bb2212639a1f
agu <agaillard@fi.uba.ar>
2021-02-08 02:08:17 -0300

    Remove gameid from test

--- curl_test.sh
+++ curl_test.sh
@@ -6,7 +6,7 @@ curl "$host/health-check" -v
 # New game
 curl -X POST "$host/game" -H "accept: application/json" -H "Content-Type: application/json" -d "{\"cols\": 5, \"rows\": 5, \"mines\": 8, \"username\": \"myuser\"}" -v
 
-gameid="58e9745e-4874-4449-a39b-a49d2e61bc84"
+gameid=""
 
 # Flag
 curl -X PUT "$host/game/$gameid/flag" -H "accept: application/json" -H "Content-Type: application/json" -d "{\"col\": 0, \"row\": 0, \"flag\": \"red_flag\", \"username\": \"myuser\"}" -v
